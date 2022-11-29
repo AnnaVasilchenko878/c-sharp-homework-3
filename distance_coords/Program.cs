190ab03cf5927ae4165f2817c2ccdc2cf2148233
Anna Vasilchenko <annavasilcenko482@gmail.com>
2022-11-29 22:20:17 +0300

    Добавляет домашние задания

---
+++ distance_coords/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B(2, 1, -7), -> 15.84
+// A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+// Радиус вектор первой точки
+Console.Write("Введите X координату радиуса-вектора первой точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату радиуса-вектора первой точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату радиуса-вектора первой точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+// Радиус вектор второй точки
+Console.Write("Введите X координату радиуса-вектора второй точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Y координату радиуса-вектора второй точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите Z координату радиуса-вектора второй точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Math.Round((Math.Sqrt(Math.Pow((x2-x1),2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2))),2));
---
+++ is_palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите любое пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number>9999 && number<=99999) {
+  int numberReverse = 0;
+  int numberCopy = number;
+  while (number > 0)
+  {
+    int digit = number % 10;
+    numberReverse = numberReverse * 10 + digit;
+    number = number / 10;
+  };
+  if (numberReverse == numberCopy) Console.WriteLine("Это палиндром!");
+  else Console.WriteLine("Это не палиндром!");
+} 
+else 
+{
+  Console.Write("Ваше число должно быть в диапазоне от 10000 до 99999 включительно");
+};
